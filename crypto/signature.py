14dbdabbc2c47298afb252a373100178e27a0297
Nikolay <nikolayostrizhny@gmail.com>
2020-12-12 09:33:13 +0300

    Implemented signature serialisation

--- crypto/signature.py
+++ crypto/signature.py
@@ -5,3 +5,16 @@ class Signature:
 
     def __repr__(self):
         return 'Signature({:x}, {:x})'.format(self.r, self.s)
+
+    def der(self):
+        rbin = self.r.to_bytes(32, 'big')
+        rbin = rbin.lstrip(b'\x00')
+        if rbin[0] & 0x80:
+            rbin = '\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+        sbin = self.r.to_bytes(32, 'big')
+        sbin = sbin.lstrip(b'\x00')
+        if sbin[0] & 0x80:
+            sbin = '\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
